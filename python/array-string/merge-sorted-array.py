a94b6865e27207f41585ea855991dd8e9c5bb26a
Reza Mobaraki <rezam578@gmail.com>
2025-09-26 21:18:08 +0330

    Add(merge-sorted-array): implement merge_v2 function for improved merging

--- python/array-string/merge-sorted-array.py
+++ python/array-string/merge-sorted-array.py
@@ -17,10 +17,25 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         k -= 1
 
 
+def merge_v2(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    p1 = m - 1
+    p2 = n - 1
+    p = m + n - 1
+    while p2 >= 0:
+        if p1 >= 0 and nums1[p1] > nums2[p2]:
+            nums1[p] = nums1[p1]
+            p1 -= 1
+        else:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+        p -= 1
+
+
 if __name__ == '__main__':
     nums1 = [2, 3, 4, 0, 0, 0]
     m = 3
     nums2 = [1, 5, 6]
     n = 3
-    merge(nums1, m, nums2, n)
+    # merge(nums1, m, nums2, n)
+    merge_v2(nums1, m, nums2, n)
     print(nums1)  # Output: [1, 2, 2, 3, 5, 6]
