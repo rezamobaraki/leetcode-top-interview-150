28a9385e5de748473d75757569c75cefa998b8e6
Reza Mobaraki <rezam578@gmail.com>
2026-02-18 16:34:09 +0100

    Add: implement function to reverse a linked list

--- python/leetcode/1-two-pointers/5_majority_element.py
+++ python/leetcode/1-two-pointers/5_majority_element.py
@@ -9,10 +9,12 @@ def majority_element(nums):
 
     return candidate
 
+def majority_element_v2(nums):
+    return sorted(nums)[len(nums) // 2]
 
 if __name__ == '__main__':
     nums1 = [3, 2, 3]
     print(majority_element(nums1))  # Output: 3
 
     nums2 = [2, 2, 1, 1, 1, 2, 2]
-    print(majority_element(nums2))  # Output: 2
+    print(majority_element_v2(nums2))  # Output: 2
